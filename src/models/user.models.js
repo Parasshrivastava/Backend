af130cc7908a9d6bec45bd9171cb0d4bbc4f1165
Paras <parasshrivastava0@gmail.com>
2024-03-14 12:40:31 +0530

    user model updated with JWT,bcrypt (hash the password)

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,8 @@
 import mongoose ,{Schema}from "mongoose";
+import { Jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+
 const userSchema=new Schema({
     username:{
         type:String,
@@ -45,5 +49,46 @@ const userSchema=new Schema({
         
     },
 },{timestamps:true})
+
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken=function(){
+    Jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPRIRY
+    }
+    
+    )
+}
+
+userSchema.methods.generateRefreshToken=function(){
+    Jwt.sign({
+        _id:this._id,
+        
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+    
+    )
+}
+
 
 export const User=mongoose.model("User",userSchema)
